d83aa509e85fde6f3a9c40916fa259431398902e
张博睿 <22373388@buaa.edu.cn>
2025-02-25 21:29:42 +0800

    选范围2

--- extensions/vscode/src/autocomplete/completionProvider.ts
+++ extensions/vscode/src/autocomplete/completionProvider.ts
@@ -269,7 +269,7 @@ export class ContinueCompletionProvider
       if (selectedCompletionInfo) {
         outcome.completion = selectedCompletionInfo.text + outcome.completion;
       }
-      outcome.completion = "<｜c> main():<c｜>Th<｜m>is is a sample text<m｜> for <｜d>wrapping<d｜>";
+      // outcome.completion = "<｜c> main():<c｜>Th<｜m>is is a sample text<m｜> for <｜d>wrapping<d｜>";
       const willDisplay = this.willDisplay(
         document,
         selectedCompletionInfo,
