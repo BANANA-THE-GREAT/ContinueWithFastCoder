08415c472bc177f54617bf81ee405316a46adf21
焦子谦 <22371047@buaa.edu.cn>
2025-03-09 00:56:55 +0800

    修改logo

--- gui/src/components/OnboardingCard/tabs/OnboardingQuickstartTab.tsx
+++ gui/src/components/OnboardingCard/tabs/OnboardingQuickstartTab.tsx
@@ -6,7 +6,8 @@ function OnboardingQuickstartTab() {
     <div className="flex h-full w-full items-center justify-center">
       <div className="xs:px-0 flex w-full max-w-full flex-col items-center justify-center px-4 text-center">
         <div className="xs:flex hidden">
-          <ContinueLogo height={75} />
+          <ContinueLogo />
+          {/* <ContinueLogo height={75} /> */}
         </div>
 
         {/* <p className="xs:w-3/4 w-full text-sm">
